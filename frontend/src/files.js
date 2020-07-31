ac4f367f30256cb232bce39b2bd2e6ab585afe44
jaiye agbonavbare <agbojai15@icloud.com>
2020-07-31 08:48:29 -0400

    no auth

--- frontend/src/files.js
+++ frontend/src/files.js
@@ -4,27 +4,36 @@ import Cloud from './images/cloud.jpg';
 
 //handle sign out submit
 
-//component did mount here (handle file listing)
-function Files(props){
-  
-  return(
-    <div>
-      <title>My Files</title>
-      <div className="Nav">
-        <img src={Cloud} className='cloud' alt=" "></img>
-        <form><button type="submit" className="sign-out">Sign Out</button></form>
-      </div>
-      <div className="content">
-        <div className="path">
-          <button>Home</button>
-        </div>
-        <div className="file-list">
+class Files extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+    
+  }
 
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <div>
+        <title>My Files</title>
+        <div className="Nav">
+          <img src={Cloud} className='cloud' alt=" "></img>
+          <form><button type="submit" className="sign-out">Sign Out</button></form>
+        </div>
+        <div className="content">
+          <div className="path">
+            <button>Home</button>
+          </div>
+          <div className="file-list">
+  
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
   
-  export default Files;
-  
+export default Files;  
