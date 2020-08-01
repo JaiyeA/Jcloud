72cafc60bc2ab217148d30fee493e0b16a2b2d19
jaiye agbonavbare <agbojai15@icloud.com>
2020-08-01 07:58:53 -0400

    Authentication handler

---
+++ frontend/src/Auth.js
@@ -0,0 +1,22 @@
+
+
+class Authentication {
+    constructor() {
+        
+    }
+
+    signIn(username, password){
+
+    }
+    //Refresh token??
+    authCheck(){
+
+    }
+
+    logOut(){
+        
+    }
+}
+
+const auth = new Authentication();
+auth.signIn('hello','world')
