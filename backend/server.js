375f2004690b9a3d846cd7799a27906ef1639903
Jaiye Agbonavbare <44385823+JaiyeA@users.noreply.github.com>
2020-08-24 14:36:51 -0400

    No Db yet

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const jose = require('jose');
+require('dotenv').config();
 
-app.get('/', (req,res) => {
-  res.send('hello world')
+const app = express();
+
+const key = process.env.SECRET_KEY;
+const jwk = jose.JWK.asKey(key);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/', (req,res) => { //login
+  const body = req.body
+  if(body.username == process.env.ADMIN_USERNAME && 
+    body.password ==process.env.ADMIN_PASSWORD){
+    //create jwt and set it in an httpOnly cookie
+    const payload = {
+      'user': 'Jaiye',
+      'admin': true,
+    }
+     const jwt = jose.JWT.sign(payload, jwk, {
+      algorithm: 'HS256',
+      expiresIn: '24 hours',
+      issuer: 'Jcloud API',
+      now: new Date(),
+      header: { typ: 'JWT', alg: 'HS256' }
+    })
+    res.setHeader('Set-Cookie', [`Auth_token=${jwt}; HttpOnly`]);   
+    res.json({authenticated: true});
+    console.log('admin has logged in at', new Date());
+  }else{
+    res.json({authenticated: false});
+  }
+  res.end();
+});
+
+app.get('/auth-check', (req,res) => {//verify access token
+  if(req.headers.cookie){
+    const jwt = req.headers.cookie.split("=")[1];
+    try{
+      const check = jose.JWT.verify(jwt, jwk, {});//logs an error if jwt is not valid
+      if(check.admin === true){
+        res.json({authenticated: true});
+      }else{
+        res.json({authenticated: false});
+      }
+    }catch{
+      res.json({authenticated: false});
+    }
+  } else{
+    res.json({authenticated: false});
+  }
+  res.end();
+});
+
+app.get('/logout', (req,res) => {
+  res.setHeader('Set-Cookie', ['Auth_token=null; HttpOnly']);   
+  res.end();
 })
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.get('get-files', (req,res)=>{
+  
+})
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`listening on port ${port}`));
